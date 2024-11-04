c6586f58b2100b0996cdd8551f1a10a43f1d6372
Omed <omed_hossaini@outlook.com>
2024-11-04 17:45:39 -0500

    added test

--- server/submitForm.js
+++ server/submitForm.js
@@ -9,6 +9,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get('/api/test', (req, res) => {
+  res.status(200).send('API is working');
+});
+
 // Server - /api/submitForm route
 app.post('/api/submitForm', async (req, res) => {
   try {
