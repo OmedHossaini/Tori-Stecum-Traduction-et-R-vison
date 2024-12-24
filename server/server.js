deffba1e47fa01819832260e80579994f73d773a
OmedHossaini <omedhossainiomedhossaini123@hotmail.com>
2024-12-24 12:22:01 -0500

    removed comments

--- server/server.js
+++ server/server.js
@@ -9,13 +9,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Server - /api/submitForm route
-app.post('/api/submitForm', async (req, res) => { // Fixed the route definition
+
+app.post('/api/submitForm', async (req, res) => {
   try {
     const formData = req.body;
     console.log('Received form data:', formData);
 
-    // Set up nodemailer transporter for Gmail
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
@@ -41,7 +40,7 @@ app.post('/api/submitForm', async (req, res) => { // Fixed the route definition
   }
 });
 
-// Start the server
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
