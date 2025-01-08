dc86a75823a40db45e318f8e26c67edd8035ed85
Omed <omed_hossaini@outlook.com>
2025-01-08 13:35:42 -0500

    removed a sentence

--- client/src/components/CopyWritingServices.js
+++ client/src/components/CopyWritingServices.js
@@ -8,7 +8,7 @@ const CopywritingServices = () => (
     <AnimatedBackground />
     <h2 className="copywriting-services-heading">Copywriting Services</h2>
     <p className="copywriting-services-paragraph">
-      Copywriting is, for those who may not know, the art of crafting persuasive and compelling written content for various mediums, such as advertisements, websites, marketing materials, and more. If you’re in need of something written from scratch, hit the contact button below!
+      Copywriting is, for those who may not know, the art of crafting persuasive and compelling written content for various mediums, such as advertisements, websites, marketing materials, and more. If you’re in need of something written from scratch!
     </p>
   </div>
 );
