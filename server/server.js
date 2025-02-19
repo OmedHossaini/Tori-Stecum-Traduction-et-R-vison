f296fe58e35e14efd039e4fd277793fd51255b21
Omed <omed_hossaini@outlook.com>
2025-02-18 22:33:42 -0500

    removed comments on server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: '*' })); // Allow all origins
+app.use(cors({ origin: '*' })); 
 app.use(express.json());
 
 app.post('/api/submitForm', async (req, res) => {
